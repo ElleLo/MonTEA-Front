39fa4b474698b78acd9a0cef9dee0a9f61d4e15e
Anna Harvey <engineering@clock13.com>
2019-04-06 11:32:52 +1100

    Login page basics

---
+++ build/precache-manifest.608ce782971228095188bf5a606d5848.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d3e1a7efce90bbbdbf0d",
+    "url": "/static/js/main.d3e1a7ef.chunk.js"
+  },
+  {
+    "revision": "a593cc5e3a9f08c16559",
+    "url": "/static/js/1.a593cc5e.chunk.js"
+  },
+  {
+    "revision": "d3e1a7efce90bbbdbf0d",
+    "url": "/static/css/main.b9a1b4a6.chunk.css"
+  },
+  {
+    "revision": "1edbcc1088d94c36515d6680522de958",
+    "url": "/index.html"
+  }
+];
--- src/login.bs.js
+++ src/login.bs.js
@@ -22,10 +22,34 @@ function make(_children) {
               return React.createElement("div", {
                           className: "App"
                         }, React.createElement("div", {
-                              className: "App-header"
-                            }, React.createElement("h2", undefined)), React.createElement("p", {
-                              className: "App-intro"
-                            }, "To get hello, edit", React.createElement("code", undefined, " src/app.re "), Utils.str("and save to reload.")));
+                              className: "container mx-auto h-full flex justify-center items-center"
+                            }, React.createElement("div", {
+                                  className: "w-1/2 text-center py-4"
+                                }, React.createElement("h1", {
+                                      className: "font-hairline mb-6 text-center"
+                                    }, Utils.str("Login to Party Finder")), React.createElement("div", {
+                                      className: "border-teal p-8 border-t-12 bg-white mb-6 rounded-lg shadow-lg"
+                                    }, React.createElement("div", {
+                                          className: "mb-4"
+                                        }, React.createElement("label", {
+                                              className: "font-bold text-grey-darker block mb-2"
+                                            }, Utils.str("Username or Email")), React.createElement("input", {
+                                              className: "block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow",
+                                              placeholder: "Your Username",
+                                              type: "text"
+                                            })), React.createElement("div", {
+                                          className: "mb-4"
+                                        }, React.createElement("label", {
+                                              className: "font-bold text-grey-darker block mb-2"
+                                            }, Utils.str("Password")), React.createElement("input", {
+                                              className: "block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow",
+                                              placeholder: "Password",
+                                              type: "text"
+                                            })), React.createElement("div", {
+                                          className: "flex items-center justify-center"
+                                        }, React.createElement("button", {
+                                              className: "block bg-teal-dark hover:bg-teal text-white font-bold py-2 px-4 rounded"
+                                            }, Utils.str("Login")))))));
             }),
           /* initialState */component[/* initialState */10],
           /* retainedProps */component[/* retainedProps */11],
